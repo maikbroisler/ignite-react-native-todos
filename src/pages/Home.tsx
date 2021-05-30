3343dea549eae4f04347c0648f4bfd57c8c145d8
Maik Broisler <maikbroislerdevs@gmail.com>
2021-05-30 14:10:08 -0300

    Desafio Concluído: Conceitos do React Native

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,35 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    
+    if(newTaskTitle) {
+      const task = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      }
+
+      setTasks(oldState => [...oldState, task]);
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+    const taskChanged = tasks.map(task => task.id === id ? {
+      ...task,
+      done: !task.done
+    } : task);
+
+    setTasks(taskChanged);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id));
   }
 
   return (
